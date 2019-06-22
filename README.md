8f9596b3bb3c442725bf8e574b40dcd91213058b
Billy <billy@billyfung.com>
2019-06-23 10:57:09 +1200

    setting up celery as well, need to dockerise that

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Flask with celery task queue that does long processing job in the background
 - use whatever external functions you'd like outside of app
 - look into how to tie into using app models if needed
+
+# setup
+Using docker for everything
+
+`docker-compose up`
+will spin up redis and flask app container
+http://localhost:5000/ contains the web app
+
+# task queue
+Celery with redis broker to do async processing
--- app/app.py
+++ app/app.py
@@ -1,13 +1,40 @@
 from flask import Flask
 from redis import Redis
+from celery import Celery
+from decimal import Decimal
 
+
+SAMPLE_DATA = {
+    'uuid1': {
+        1: Decimal(1.2),
+        2: Decimal(1.5),
+        3: Decimal(0.3),
+        4: Decimal(0.1)
+    },
+    'uuid2': {
+        1: Decimal(0.2),
+        2: Decimal(0.5),
+        3: Decimal(1.3),
+        4: Decimal(1.1)
+    }
+}
+
+# initialise the app
+# TODO: use envars for hosts
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+celery = Celery(broker='redis://localhost:6379/0')
+
 
 @app.route('/')
 def hello():
     redis.incr('hits')
     return 'This has been viewed %s time(s).' % redis.get('hits')
+
+@celery.task(name='calc.processing')
+def processing():
+    from external_lib import process_data
+    output = process_data(SAMPLE_DATA)
 
 
 if __name__ == "__main__":
