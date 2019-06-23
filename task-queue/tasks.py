a95dad84215d47a0717a1e1d126ce6e5d6d2a2b4
Billy <billy@billyfung.com>
2019-06-24 08:23:33 +1200

    linting

--- task-queue/tasks.py
+++ task-queue/tasks.py
@@ -3,10 +3,20 @@ import time
 from celery import Celery
 
 
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
+CELERY_BROKER_URL = os.environ.get(
+    'CELERY_BROKER_URL',
+    'redis://localhost:6379'
+)
+CELERY_RESULT_BACKEND = os.environ.get(
+    'CELERY_RESULT_BACKEND', 
+    'redis://localhost:6379'
+)
 
-celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+celery = Celery(
+            'tasks', 
+            broker=CELERY_BROKER_URL, 
+            backend=CELERY_RESULT_BACKEND
+)
 
 
 @celery.task(name='tasks.process')
