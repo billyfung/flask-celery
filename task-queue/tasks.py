3ec74f084219aeff2fb833f2a9afd5012ffc25b4
Billy <billy@billyfung.com>
2019-07-02 14:16:11 +1200

    updating task to insert into db

--- task-queue/tasks.py
+++ task-queue/tasks.py
@@ -1,15 +1,17 @@
 import os
 import time
 from celery import Celery
+import psycopg2
+from random import randint
 
 
 CELERY_BROKER_URL = os.environ.get(
     'CELERY_BROKER_URL',
-    'redis://localhost:6379'
+    'redis://redis:6379'
 )
 CELERY_RESULT_BACKEND = os.environ.get(
     'CELERY_RESULT_BACKEND', 
-    'redis://localhost:6379'
+    'redis://redis:6379'
 )
 
 celery = Celery(
@@ -18,9 +20,24 @@ celery = Celery(
             backend=CELERY_RESULT_BACKEND
 )
 
+DB_CONN = "dbname=testdb user=testuser password=password host=db"
+INSERT_SQL = """
+    insert into test_table (col_a, col_b, test_value) 
+    values (%s, %s)
+"""
 
-@celery.task(name='tasks.process')
+@celery.task(name='tasks.process', ignore_result=True)
 def process(input_data):
-    time.sleep(5)
-    output_data = input_data
-    return output_data
+    time.sleep(1)
+    A = int(input_data['A'])
+    B = int(input_data['B'])
+    C = int(input_data['C'])
+    with psycopg2.connect(DB_CONN) as conn:
+        with conn.cursor() as c:
+            c.execute(
+                INSERT_SQL, 
+                (
+                    A*randint(1,100), 
+                    B*randint(1,100), 
+                    C*randint(1,30))
+            )
